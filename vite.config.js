b4ad8fe6170a97878cbbbfcd792dde8dee149d07
kratigya001 <kratigyabansal80@gmail.com>
2025-11-03 14:19:54 +0530

    versel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,4 +10,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: "dist",
+  },
+  // 👇 This is the key line for client-side routing (React Router)
+  server: {
+    historyApiFallback: true,
+  },
+});
